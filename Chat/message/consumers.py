7c4b5c78600a13e6a720aa888f7a0348f424e195
nataliarestrada <nati.tachito@gmail.com>
2022-07-18 01:08:47 -0300

    channels redis

--- Chat/message/consumers.py
+++ Chat/message/consumers.py
@@ -1,20 +1,44 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from .models import Message
+from .api.serializers import MessageSerializer
+from asgiref.sync import async_to_sync
 
 
 class MessageConsumer(WebsocketConsumer):
     def connect(self):
-        self.accept()
         self.chat_id = self.scope['url_route']['kwargs']['chat_id']
-        print("Chat ID: ", self.chat_id)
+        self.user = self.scope['user']
+        self.chat_name = 'chat_'+str(self.chat_id)
+        print(self.user)
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.chat_name,
+            self.channel_name
+        )
+        self.accept()
 
     def disconnect(self, code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(
+            self.chat_name,
+            self.channel_name
+        )
 
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-
-        print("Message received", message)
-
-        self.send(text_data=json.dumps({'message': message}))
+        action = text_data_json["action"]
+        if action == "send_message":
+            message = text_data_json["message"]
+            new_message = Message.objects.create(content=message, author=self.user, idChat_id=self.chat_id)
+            serialized_message = MessageSerializer(new_message)
+    
+            async_to_sync(self.channel_layer.group_send)(
+                self.chat_name, {
+                    'type': "send_message",
+                    'message': serialized_message.data
+                }
+            )
+            # self.send(text_data=json.dumps())
+    
+    def send_message(self, event):
+        self.send(text_data=json.dumps(event))
